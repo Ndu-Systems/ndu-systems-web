0b99f69e3e888dd97aa99c100ff55d7562ff3982
freedomkhanyile <freedom.khanyile1@gmail.com>
2019-03-13 22:10:27 +0200

    map section created

--- src/app/home/index.ts
+++ src/app/home/index.ts
@@ -1,3 +1,4 @@
 export * from './home.component';
 export * from './home-header/home-header.component';
 export * from './home-banner/home-banner.component';
+export * from './home-map/home-map.component';
