06106834b2c74467bb33ab9010dda67b83b7781e
freedomkhanyile <freedom.khanyile1@gmail.com>
2019-03-24 01:11:00 +0200

    Responsive till Iphone 8

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,7 +4,7 @@ import { NavigationEventEmiter } from '../navigation/navigationEventEmiter';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
   showNav: boolean;
