2f25d14d91b4ace5eedd7df612aabcedd1160c68
NdumisoMthembu <mrnnmthembu@gmail.com>
2019-03-26 21:37:57 +0200

    Create services page

--- src/app/our-services/our-services.component.ts
+++ src/app/our-services/our-services.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-our-services',
   templateUrl: './our-services.component.html',
-  styleUrls: ['./our-services.component.css']
+  styleUrls: ['./our-services.component.scss']
 })
 export class OurServicesComponent implements OnInit {
 
