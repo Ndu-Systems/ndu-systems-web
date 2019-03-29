d18c5e25da359f8c6ab4fb63a9f519333b85de8b
NdumisoMthembu <mrnnmthembu@gmail.com>
2019-03-29 21:42:14 +0200

    scroll up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 import { NavigationEventEmiter } from './navigation/navigationEventEmiter';
  
 
@@ -7,10 +8,21 @@ import { NavigationEventEmiter } from './navigation/navigationEventEmiter';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private router:Router) { 
+  }
+  ngOnInit(){
+    
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+          return;
+      }
+      window.scrollTo(0, 0)
+  });
+  }
   title = 'ndu-systems-web';
   showNav: boolean;
-  constructor() { }
+
 
 openNav(event: NavigationEventEmiter) {
     if (!event.closeNav) {
